d223043bf7d739fd3337b24d08b16e323c9d2360
Rumman Ahmed <ahmed.rumman@gmail.com>
2014-09-17 12:09:41 +1000

    Mouse move transfered

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -3,11 +3,12 @@
 angular.module('nodeAppApp')
   .controller('MainCtrl', function ($scope, $http, socket) {
     $scope.awesomeThings = [];
+    $scope.spy_marker = $("#spy_marker");
 
     $http.get('/api/things').success(function(awesomeThings) {
       $scope.awesomeThings = awesomeThings;
       socket.syncUpdates('thing', $scope.awesomeThings);
-    });
+      });
 
     $scope.addThing = function() {
       if($scope.newThing === '') {
@@ -15,13 +16,15 @@ angular.module('nodeAppApp')
       }
       $http.post('/api/things', { name: $scope.newThing });
       $scope.newThing = '';
-    };
+      };
+    $scope.deleteThing = function(thing) {
+      $http.delete('/api/things/' + thing._id);
+      };
     $scope.sendSock =function(){
      $("body").bind("mousemove",function(evt){
       socket.socket.emit("sendMouse",evt.pageX,evt.pageY);
       });
-    };
-
+      };
     $scope.spy =function(move){
       $scope.spy_marker.show().css({
         left:(move.x -2) + "px",
@@ -29,23 +32,9 @@ angular.module('nodeAppApp')
       });
      };
 
-    socket.socket.on("sendMouse",function(move){
-       $("#spy_marker").show().css({
-        left:(move.x -2) + "px",
-        right:(move.y -2) + "px"
-      });
-        console.log("x is " + move.x  +" and y is " + move.y );
-    }
-      // $scope.spy
-      );
-
-    $scope.spy_marker = $("#spy_marker");
-
-    $scope.deleteThing = function(thing) {
-      $http.delete('/api/things/' + thing._id);
-    };
-
+    socket.socket.on("sendMouse",$scope.spy);
     $scope.$on('$destroy', function () {
       socket.unsyncUpdates('thing');
-    });
+      });
+    
   });
