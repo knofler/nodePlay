8f820a5426818d6200e3301d0b9e250cd510a154
Rumman Ahmed <ahmed.rumman@gmail.com>
2014-09-23 18:17:13 +1000

    media player building process ongoing

--- client/app/audio/audio.controller.js
+++ client/app/audio/audio.controller.js
@@ -15,6 +15,10 @@ angular.module('nodeAppApp')
 
   	//access current audioplay properties directly from API
   	$scope.showCurrentTime;
+  	$scope.mediaDuration;
+  	$scope.audioPlay.playbackRate = 1.0;
+  	$scope.audioPlay.volume = 1.0;
+  	$scope.audioPlay.ended;
 
   	//Custom start stop and other button element selector
   	$scope.recordButton 	  = document.querySelector("button#recordStart");
@@ -78,7 +82,7 @@ angular.module('nodeAppApp')
 	  }; 
 
     //Audio Stream call back and error handling	
-	$scope.gotAudioStream =function(stream) {
+	$scope.gotAudioStream 	 =function(stream) {
 	   var videoTracks = stream.getVideoTracks();
 	   var audioTracks = stream.getAudioTracks();
 	    if (audioTracks.length == 1 && videoTracks.length == 0) {
@@ -125,7 +129,7 @@ angular.module('nodeAppApp')
 	    $scope.stopRecordButton.disabled = true;
 	    alert('Failed to get access to local media. Error code: ' + error.code);
  	 }; 	  
-	$scope.paintMeter = function(context, number) {
+	$scope.paintMeter 		 = function(context, number) {
      context.clearRect(0, 0, 400, 20);
      context.fillStyle = 'red';
      context.fillRect(0, 0, number * 400, 20);
@@ -137,7 +141,7 @@ angular.module('nodeAppApp')
 	   $scope.recordButton.disabled = true;
        $scope.stopRecordButton.disabled = false;
 	 }; 
-	$scope.recordStop = function (){
+	$scope.recordStop  = function (){
 	   $scope.recordButton.enabled = true;
        $scope.stopRecordButton.enabled = false;
 	   $scope.localStream.stop();
@@ -146,48 +150,74 @@ angular.module('nodeAppApp')
 	 }; 
 
 	//Play Local audio media start ,stop, pause and other control  
-  	$scope.playPause = function(){
+  	$scope.playPause 		 = function(){
   		$scope.audioPlay.pause();
   	 };
-  	$scope.playStart = function(){
+  	$scope.playStart 		 = function(){
   		$scope.audioPlay.play();
   	 };
-  	$scope.playStop = function(){
+  	$scope.playStop 		 = function(){
   		$scope.audioPlay.pause();
   		$scope.audioPlay.currentTime = 0;
   	 };
-  	$scope.supportType = function(vidType,codType){
+  	$scope.supportType 		 = function(vidType,codType){
   		var answer = $scope.audioPlay.canPlayType(vidType+';codecs="'+codType+'"');
  		document.querySelector("#resultDiv").innerHTML = "<hr><p> Result of canPlayType is : <b>" + angular.uppercase(answer)+"</b></p>";
   	 }; 
-  	$scope.bufferedInfo = function(){
+  	$scope.bufferedInfo 	 = function(){
   		var start,end,length;
   		start = $scope.audioPlay.buffered.start(0);
   		end = $scope.audioPlay.buffered.end(0);
   		document.querySelector("#resultDiv").innerHTML = "<hr><p> Buffered started at  : <b>" +start+"</b> Ended at <b>" + end + " </b></p>";
   	 };
-  	$scope.mediaSource = function(){
+  	$scope.mediaSource 		 = function(){
   		var src= $scope.audioPlay.currentSrc;
   		document.querySelector("#resultDiv").innerHTML = "<hr><p> Current media source is  : <b>" +src+"</b></p>";
   	 }; 
-
+  	$scope.showMediaDuration = function(){
+  		$scope.mediaDuration = $scope.audioPlay.duration;
+  		document.querySelector("#audioStaticinfo").innerHTML = "<hr><p> Media Duration is  : <b>" + $scope.mediaDuration +"</b></p>";
+  		console.log($scope.mediaDuration);
+  	 }; 
+  	$scope.playFast 		 = function(){
+  		$scope.audioPlay.playbackRate+= 0.5
+  	 }; 
+  	$scope.slowMotion 		 = function(){
+  		$scope.audioPlay.playbackRate-= 0.5
+  	 };
+  	$scope.playEnd 			 = function(){
+  		$scope.audioPlay.currentTime=$scope.audioPlay.duration;
+  	 }; 
+  	$scope.playRepeat 		 = function(){
+	  var repeat= $scope.audioPlay.loop;
+		  if(repeat == false){
+		  	$scope.audioPlay.loop = true;
+		  } else{
+		  	$scope.audioPlay.loop = false;
+		  }	
+  	 }; 
 
   	//Web Audio Events
-  		$scope.audioPlay.addEventListener('timeupdate',function(){
-  		$scope.showCurrentTime = $scope.audioPlay.currentTime ;
-  		document.querySelector("#audioInfo").innerHTML = "<p>Current Media time is : <span> <b>" + $scope.showCurrentTime +"</b></span></p>";
-  		// console.log($scope.showCurrentTime)
-  	  });
+  	$scope.audioPlay.addEventListener('timeupdate',function(){
+  			$scope.showCurrentTime = $scope.audioPlay.currentTime ;
+	  		document.querySelector("#audioInfo").innerHTML = "<p>Current Media time is : <span> <b>" + $scope.showCurrentTime +"</b></span><br />"+
+	  		"Current Media play rate is <span> <b>" + $scope.audioPlay.playbackRate + "</b></span><br />"+
+	  		"Repeat option enable is <span> <b>"+ $scope.audioPlay.loop + "</b></span></p>";
+	  		// console.log($scope.showCurrentTime)
+	  	  });
+  	$scope.audioPlay.addEventListener('ended',function(){
+  		document.querySelector("#audioInfo").innerHTML = "<p>Media Play ended status is : <span> <b>" + $scope.audioPlay.ended +"</b></span><br />"
+  	 });
   	
-
-
  	//Audio Context onLoad Event
 	$scope.onload = function() {
 	 try {
 	      window.AudioContext = window.AudioContext || window.webkitAudioContext;
 	      $scope.audioContext = new AudioContext();
+	      setTimeout(function(){
+	      	$scope.showMediaDuration();	
+	      },500)   
 	    } catch(e) {
-	     console.log('Web Audio API not found');
 	    }
 	 };   
 
